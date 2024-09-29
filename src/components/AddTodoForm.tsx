165bdbf54aa875d8a9f3251615aa44a4e4e630ff
AaronIanWilson <Aaron.I.W@proton.me>
2024-09-29 20:38:41 +0100

    add a maximum character count to form input / placeholder

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -17,8 +17,10 @@ export default function AddTodoForm() {
       <h2 className="font-medium text[#231d15]">Add a todo</h2>
       <input
         type="text"
+        placeholder="Enter text"
         className="h-[45px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full px-[16px]"
         value={todoText}
+        maxLength={40}
         onChange={(e) => {
           setTodoText(e.target.value);
         }}
